5c8d2103bcd98d78b3801d77b460fa037a3d1192
leonid <orange59usa@gmail.com>
2023-02-15 01:37:59 +0300

    END

--- Program.cs
+++ Program.cs
@@ -1,6 +1,28 @@
 ﻿Console.WriteLine("начало работы");
 string[] array = {"txt", "one", "R2D2", "Leo"};
+string[] newArray =new string[4];
+int count = 0;
+
+
+
 for (int i = 0; i < array.Length; i++)
 {
-    Console.WriteLine(array[i]);
+    int length = array[i].Length;
+
+    if (length <= 3)
+    {
+        string s = array[i];
+        //newArray[count] = array[i];
+        newArray[count] = s;
+        count++;
+    }
+
 }
+
+for (int i = 0; i < newArray.Length; i++)
+{
+    Console.WriteLine(newArray[i]);
+
+}
+
+Console.WriteLine("спасибо за просмотр!! привет с Новороссийска");
